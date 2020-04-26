4b7232efb2fd66db4bb4e809bf56a1358cc0ce93
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-04-26 21:00:38 +0100

    improvement: remove webpack from slices
    all slices must use only principal webpack project

--- slices/footer/webpack/props/output.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const output_base = {
-    path: path.resolve(__dirname, '../../../../apps/slices-build/footer'),
-    filename: `main.[hash].js`,
-    publicPath: '/',
-};
-
-module.exports = { output_base };
--- slices/header/webpack/webpack.config.prod.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.config.common');
-const { pluginProd } = require('./props/plugins');
-
-module.exports = merge(common, {
-    devtool: 'source-map',
-    plugins: pluginProd,
-});
