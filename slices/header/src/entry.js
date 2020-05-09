98d77551930044fef6fde681f6620ea6698313e3
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-05-09 13:09:07 +0100

    feat: add a new scope for ssr in slice

--- slices/header/src/entry.js
+++ slices/header/src/entry.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
 import App from './layout/app';
 
-render(<App />, document.getElementById('slice-header'));
+const Entry = () => <App />;
+
+export default Entry;
